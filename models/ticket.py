4aae8df1c037d7cf17248e69fa889c37c880a166
André Oliveira <andreaoliveira@ua.pt>
2024-10-15 19:26:22 +0100

    setup done

--- models/ticket.py
+++ models/ticket.py
@@ -7,13 +7,13 @@ from db.database import Base
 
 
 class Ticket(Base):
-    __tablename__ = 'tickets' 
+    __tablename__ = "tickets"
 
     id = Column(Integer, primary_key=True, index=True)
-    game_id = Column(Integer) 
-    name = Column(String)
-    description = Column(Text)
-    status = Column(Boolean)
-    type = Column(String)
-    assigned_to = Column(String)
-    price = Column(Float)
+    game_id = Column(Integer, nullable=False)
+    name = Column(String(200), index=True, nullable=False)
+    description = Column(String(500), nullable=True)
+    status = Column(Boolean, default=True, nullable=False)
+    type = Column(String(50), nullable=False)
+    assigned_to = Column(String(50), nullable=True)
+    price = Column(Float, nullable=False)
