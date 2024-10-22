412fdeb333ed263a6e926177b0e58611815f4080
AlexandreCotorobai <alexandrecotorobai@gmail.com>
2024-10-22 01:34:45 +0100

    SCRUM-39 Testing finished

---
+++ tests/crud/test_crud.py
@@ -0,0 +1,167 @@
+import logging
+import pytest
+from unittest.mock import MagicMock
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from testcontainers.mysql import MySqlContainer
+
+from db.database import get_db
+from main import app
+
+
+from models.ticket import Ticket
+from models.userticket import UserTicket
+from schemas.ticket import TicketCreate, TicketUpdate, TicketInDB
+from schemas.userticket import UserTicketCreate, UserTicketUpdate, UserTicketInDB
+from crud.ticket import (
+    post_ticket,
+    buy_ticket,
+    get_tickets_by_user_id,
+    get_ticket_by_id,
+    get_tickets_by_game_id,
+    get_tickets
+)
+
+def test_post_ticket():
+    mock_db = MagicMock(spec=Session)
+
+    ticket_data = TicketCreate(
+        game_id=1,
+        name="Championship Finals",
+        description="Final match of the championship",
+        status=True,
+        type="Socio",
+        price=150.0
+    )
+
+    result = post_ticket(mock_db, ticket_data)
+
+    mock_db.add.assert_called_once()
+    mock_db.commit.assert_called_once()
+    mock_db.refresh.assert_called_once()
+
+    assert isinstance(result, Ticket)
+
+
+def test_buy_ticket():
+    mock_db = MagicMock(spec=Session)
+
+    user_ticket_data = UserTicketCreate(
+        user_id=1,
+        ticket_id=99,
+        quantity=2,
+        total_price=300.0,
+        created_at="2023-10-01T12:00:00",
+        updated_at="2023-10-01T12:00:00"
+    )
+
+    result = buy_ticket(mock_db, user_ticket_data)
+
+    mock_db.add.assert_called_once()
+    mock_db.commit.assert_called_once()
+    mock_db.refresh.assert_called_once()
+
+    assert isinstance(result, UserTicket)
+
+def test_get_tickets_by_user_id():
+    mock_db = MagicMock(spec=Session)
+
+    mock_db.query().filter().all.return_value = [
+        UserTicketInDB(
+        id=1,
+        user_id=1,
+        ticket_id=99,
+        quantity=2,
+        total_price=300.0,
+        created_at="2023-10-01T12:00:00",
+        updated_at="2023-10-01T12:00:00"
+    ),
+        UserTicketInDB(
+        id=2,
+        user_id=1,
+        ticket_id=100,
+        quantity=2,
+        total_price=100.0,
+        created_at="2023-10-01T12:00:00",
+        updated_at="2023-10-01T12:00:00"    
+    )
+    ]
+
+    result = get_tickets_by_user_id(mock_db, user_id=1)
+
+    assert isinstance(result, list)
+    assert len(result) == 2
+    assert result == [
+        UserTicketInDB(
+            id=1,
+            user_id=1,
+            ticket_id=99,
+            quantity=2,
+            total_price=300.0,
+            created_at="2023-10-01T12:00:00",
+            updated_at="2023-10-01T12:00:00"
+        ),
+        UserTicketInDB(
+            id=2,
+            user_id=1,
+            ticket_id=100,
+            quantity=2,
+            total_price=100.0,
+            created_at="2023-10-01T12:00:00",
+            updated_at="2023-10-01T12:00:00"
+        )
+    ]
+    assert all(isinstance(ticket, UserTicketInDB) for ticket in result)
+
+
+def test_get_ticket_by_id():
+    mock_db = MagicMock(spec=Session)
+
+    mock_db.query().filter().first.return_value = TicketInDB(
+        id=99, game_id=1, name="Championship Finals", description="Final match", status=True, type="Socio", price=150.0
+    )
+
+    result = get_ticket_by_id(mock_db, ticket_id=99)
+
+    # mock_db.query().filter.assert_called_once()
+    # mock_db.query.assert_called_once_with(Ticket)
+
+    assert isinstance(result, TicketInDB)
+    assert result.id == 99
+
+def test_get_tickets_by_game_id():
+    mock_db = MagicMock(spec=Session)
+
+    mock_db.query().filter().all.return_value = [
+        TicketInDB(id=99, game_id=1, name="Championship Finals", description="Final match", status=True, type="Socio", price=150.0)
+    ]
+
+    result = get_tickets_by_game_id(mock_db, game_id=1)
+
+    # mock_db.query.assert_called_once_with(Ticket)
+    # mock_db.query().filter.assert_called_once()
+
+    assert isinstance(result, list)
+    assert len(result) == 1
+    assert all(isinstance(ticket, TicketInDB) for ticket in result)
+
+def test_get_tickets():
+    mock_db = MagicMock(spec=Session)
+
+    mock_db.query().offset().limit().all.return_value = [
+        TicketInDB(id=99, game_id=1, name="Championship Finals", description="Final match", status=True, type="Socio", price=150.0),
+        TicketInDB(id=100, game_id=2, name="Championship Finals", description="Final match", status=True, type="Socio", price=150.0)
+    ]
+
+    result = get_tickets(mock_db, skip=0, limit=100)
+
+    # mock_db.query.assert_called_once_with(Ticket)
+    # mock_db.query().offset.assert_called_once_with(0)
+    # mock_db.query().offset().limit.assert_called_once_with(100)
+
+    assert isinstance(result, list)
+    assert len(result) == 2
+    assert all(isinstance(ticket, TicketInDB) for ticket in result)
--- tests/routers/test_ticket.py
+++ tests/routers/test_ticket.py
@@ -13,9 +13,6 @@ from schemas.ticket import TicketCreate, TicketUpdate, TicketInDB
 from schemas.userticket import UserTicketCreate, UserTicketUpdate, UserTicketInDB
 
 
-load_dotenv()
-REDIRECT_URI = os.environ.get("REDIRECT_URI")
-
 client = TestClient(app)
 
 @pytest.fixture(scope="module")
@@ -23,6 +20,10 @@ def mock_db():
     db = MagicMock(spec=Session)
     app.dependency_overrides[get_db] = lambda: db
     yield db
+
+@pytest.fixture(autouse=True)
+def reset_mock_db(mock_db):
+    mock_db.reset_mock()
 
 
 ticket_data = {
@@ -45,77 +46,62 @@ user_ticket_data = {
     "updated_at": "2023-10-01T12:00:00"
 }
 
-
-@pytest.fixture(autouse=True)
-def reset_mock_db(mock_db):
-    mock_db.reset_mock()
-
-
-# Test posting a ticket
-@patch("crud.ticket.post_ticket", return_value=TicketInDB(**ticket_data))
+@patch("routers.ticket.post_ticket", return_value=TicketInDB(**ticket_data))
 def test_post_ticket(mock_post_ticket, mock_db):
-    new_ticket = TicketCreate(game_id=1, name="Championship Finals", description="Final match of the championship", status=True, type="Socio", price=150.0)
-    response = client.post("/tickets", json=new_ticket.dict())
-    print(response.json())
-    # assert response.status_code == 200
-    # assert response.json() == ticket_data
-    # mock_post_ticket.assert_called_once_with(mock_db, new_ticket)
+    response = client.post("/tickets", json=ticket_data)
+    
+    assert response.status_code == 200
+    assert response.json() == ticket_data
+    mock_post_ticket.assert_called_once_with(mock_db, TicketCreate(**ticket_data))
 
-# # Test buying a ticket
-# @patch("crud.ticket.buy_ticket", return_value=UserTicketInDB(**user_ticket_data))
-# def test_buy_ticket(mock_buy_ticket, mock_db):
-#     new_user_ticket = UserTicketCreate(user_id=1, ticket_id=1)
-#     response = client.post("/tickets/buy", json=new_user_ticket.dict())
-
-#     assert response.status_code == 200
-#     assert response.json() == user_ticket_data
-#     mock_buy_ticket.assert_called_once_with(mock_db, new_user_ticket)
-
-# # Test getting tickets by user ID
-# @patch("crud.ticket.get_tickets_by_user_id", return_value=[UserTicketInDB(**user_ticket_data)])
-# def test_get_tickets_by_user_id(mock_get_tickets_by_user_id, mock_db):
-#     user_id = 1
-#     response = client.get(f"/tickets/user/{user_id}")
-
-#     assert response.status_code == 200
-#     assert response.json() == [user_ticket_data]
-#     mock_get_tickets_by_user_id.assert_called_once_with(mock_db, user_id)
-
-# # Test getting a ticket by ticket ID
-# @patch("crud.ticket.get_ticket_by_id", return_value=TicketInDB(**ticket_data))
-# def test_get_ticket_by_id(mock_get_ticket_by_id, mock_db):
-#     ticket_id = 1
-#     response = client.get(f"/tickets/{ticket_id}")
-
-#     assert response.status_code == 200
-#     assert response.json() == ticket_data
-#     mock_get_ticket_by_id.assert_called_once_with(mock_db, ticket_id)
-
-# # Test getting a ticket by ID (not found)
-# @patch("crud.ticket.get_ticket_by_id", return_value=None)
-# def test_get_ticket_by_id_not_found(mock_get_ticket_by_id, mock_db):
-#     ticket_id = 999
-#     response = client.get(f"/tickets/{ticket_id}")
-
-#     assert response.status_code == 404
-#     assert response.json() == {"detail": "Ticket not found"}
-#     mock_get_ticket_by_id.assert_called_once_with(mock_db, ticket_id)
-
-# # Test getting tickets by game ID
-# @patch("crud.ticket.get_tickets_by_game_id", return_value=[TicketInDB(**ticket_data)])
-# def test_get_tickets_by_game_id(mock_get_tickets_by_game_id, mock_db):
-#     game_id = 123
-#     response = client.get(f"/tickets/game/{game_id}")
-
-#     assert response.status_code == 200
-#     assert response.json() == [ticket_data]
-#     mock_get_tickets_by_game_id.assert_called_once_with(mock_db, game_id)
-
-# # Test getting all tickets (with pagination)
-@patch("crud.ticket.get_tickets", return_value=[TicketInDB(**ticket_data)])
-def test_get_tickets(mock_get_tickets, mock_db):
-    response = client.get("/tickets?skip=0&limit=100")
+@patch("routers.ticket.buy_ticket", return_value=UserTicketInDB(**user_ticket_data))
+def test_buy_ticket(mock_buy_ticket, mock_db):
+    response = client.post("/tickets/buy", json=user_ticket_data)
 
     assert response.status_code == 200
-    assert response.json() == []
-    # mock_get_tickets.assert_called_once_with(mock_db, 0, 100)
+    assert response.json() == user_ticket_data
+    mock_buy_ticket.assert_called_once_with(mock_db, UserTicketCreate(**user_ticket_data))
+
+@patch("routers.ticket.get_tickets_by_user_id", return_value=[UserTicketInDB(**user_ticket_data)])
+def test_get_tickets_by_user_id(mock_get_tickets_by_user_id, mock_db):
+    user_id = 1
+    response = client.get(f"/tickets/user/{user_id}")
+
+    assert response.status_code == 200
+    assert response.json() == [user_ticket_data]
+    mock_get_tickets_by_user_id.assert_called_once_with(mock_db, user_id)
+
+@patch("routers.ticket.get_ticket_by_id", return_value=TicketInDB(**ticket_data))
+def test_get_ticket_by_id(mock_get_ticket_by_id, mock_db):
+    ticket_id = 1
+    response = client.get(f"/tickets/{ticket_id}")
+
+    assert response.status_code == 200
+    assert response.json() == ticket_data
+    mock_get_ticket_by_id.assert_called_once_with(mock_db, ticket_id)
+
+@patch("routers.ticket.get_ticket_by_id", return_value=None)
+def test_get_ticket_by_id_not_found(mock_get_ticket_by_id, mock_db):
+    ticket_id = 999
+    response = client.get(f"/tickets/{ticket_id}")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Ticket not found"}
+    mock_get_ticket_by_id.assert_called_once_with(mock_db, ticket_id)
+
+@patch("routers.ticket.get_tickets_by_game_id", return_value=[TicketInDB(**ticket_data)])
+def test_get_tickets_by_game_id(mock_get_tickets_by_game_id, mock_db):
+    game_id = 123
+    response = client.get(f"/tickets/game/{game_id}")
+
+    assert response.status_code == 200
+    assert response.json() == [ticket_data]
+    mock_get_tickets_by_game_id.assert_called_once_with(mock_db, game_id)
+
+@patch("routers.ticket.get_tickets", return_value=[TicketInDB(**ticket_data)])
+def test_get_tickets(mock_get_tickets):
+    response = client.get("/tickets")
+
+    assert response.status_code == 200
+    assert response.json() == [ticket_data]
+    mock_get_tickets.assert_called_once()
